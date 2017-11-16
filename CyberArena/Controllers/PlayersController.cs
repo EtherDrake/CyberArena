6dfd565241d7f213841a9f732702df6a0f77b0ac
EtherDrake <you@example.com>
2017-11-16 14:57:57 +0200

    Starting

---
+++ CyberArena/Controllers/PlayersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CyberArena.DAL;
+using CyberArena.Models;
+
+namespace CyberArena.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private ArenaContext db = new ArenaContext();
+
+        // GET: Players
+        public ActionResult Index()
+        {
+            return View(db.Players.ToList());
+        }
+
+        // GET: Players/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // GET: Players/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Players/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PlayerID,LastName,FirstName,Nickname,Discipline,MMR,TeamID")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Players.Add(player);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(player);
+        }
+
+        // GET: Players/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PlayerID,LastName,FirstName,Nickname,Discipline,MMR,TeamID")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(player).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(player);
+        }
+
+        // GET: Players/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Players.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Player player = db.Players.Find(id);
+            db.Players.Remove(player);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CyberArena/DAL/ArenaInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CyberArena.Models;
+
+namespace CyberArena.DAL
+{
+    public class ArenaInitializer:System.Data.Entity.DropCreateDatabaseIfModelChanges<ArenaContext>
+    {
+        protected override void Seed(ArenaContext context)
+        {
+            var students = new List<Player>
+            {
+                new Player{FirstName="Danil",LastName="Ishutin", Nickname="Dendi",Discipline="Dota 2", MMR=7526, TeamID=1},
+                new Player{FirstName="Victor",LastName="Nigrini", Nickname="GeneRaL",Discipline="Dota 2", MMR=8402, TeamID=1},
+                new Player{FirstName="Vladislav",LastName="Krystanek", Nickname="Crystallize",Discipline="Dota 2", MMR=8662, TeamID=1},
+                new Player{FirstName="Vladimir",LastName="Nikogosyan ", Nickname="RodjER",Discipline="Dota 2", MMR=8072, TeamID=1},
+                new Player{FirstName="Akbar",LastName="Butaev", Nickname="SoNNeikO",Discipline="Dota 2", MMR=9470, TeamID=1},
+                new Player{FirstName="Dmytro",LastName="Filipchuk", Nickname="DIMAGA",Discipline="Starcraft 2", MMR=7323, TeamID=2},
+                new Player{FirstName="Vitaliy",LastName="Ponomarenko", Nickname="Minato",Discipline="Starcraft 2", MMR=7526, TeamID=2},
+            };
+
+            students.ForEach(s => context.Players.Add(s));
+            context.SaveChanges();
+            var courses = new List<Team>
+            {
+                new Team{Name="Na'Vi", Rating=3000},
+                new Team{Name="Ir0nChain", Rating=2500},
+            };
+            courses.ForEach(s => context.Teams.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
